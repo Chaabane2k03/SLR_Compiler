0c3a817e7276b73285805a9fff686ed9ecce416d
Chaabane2k03 <chaabaneboussadia@gmail.com>
2024-11-30 11:26:50 +0100

    Initialising project

---
+++ compilerIGL3/src/main/java/com/mycompany/compilerigl3/CompilerIGL3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compilerigl3;
+
+/**
+ *
+ * @author MSI
+ */
+public class CompilerIGL3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
