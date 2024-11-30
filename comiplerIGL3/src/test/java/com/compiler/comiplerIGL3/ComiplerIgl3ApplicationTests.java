3040628f49ded94097542a569a0f6aa4618ffc51
Chaabane2k03 <chaabaneboussadia@gmail.com>
2024-11-30 11:16:01 +0100

    adding Java Work

---
+++ comiplerIGL3/src/test/java/com/compiler/comiplerIGL3/ComiplerIgl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compiler.comiplerIGL3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComiplerIgl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
