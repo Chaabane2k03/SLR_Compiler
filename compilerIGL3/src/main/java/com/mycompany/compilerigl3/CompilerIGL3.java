e394398ffa0f09c44fcd008d126d5c1be57af82d
Chaabane2k03 <chaabaneboussadia@gmail.com>
2024-12-05 08:37:48 +0100

    fix sth

--- compilerIGL3/src/main/java/com/mycompany/compilerigl3/CompilerIGL3.java
+++ compilerIGL3/src/main/java/com/mycompany/compilerigl3/CompilerIGL3.java
@@ -13,28 +13,10 @@ public class CompilerIGL3 {
 
     public static void main(String[] args) {
         // Hardcoded file path
-        String filePath = "D:\\Raed\\Desktop\\SLR_Compiler\\code.txt"; // copy it from the windows copy as path
-
-        System.out.println("** Using file path: " + filePath);
-
-        try {
-            // Initialize your custom Scanner with the hardcoded file path
-            Scanner scanner = new Scanner(filePath);
-            
-            // Use the toString() method to get the file content as a string
-            String code = scanner.toString();
-            
-            // Lexical analysis
-            LexicalParser lexer = new LexicalParser(code);
-            List<Token> tokens = lexer.parse();
-
-            // Parsing
-            Parsernew parser = new Parsernew();
-            parser.analyzeSLnew(tokens);
-
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            e.printStackTrace();
+        if (args.length < 1)
+        {
+            System.out.print("no file provided");
+            return;
         }
         
         System.out.println("**"+args[0]);
